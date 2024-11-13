036e7234655731f40ea26922a3714f79f5bb5294
zAdmT <adamthomson079@gmail.com>
2024-11-13 18:48:29 +0000

    first fix implemented

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,11 +5,11 @@ class Dec2Hex
 public static int Arg1;
     public static void main(String args[])    {
         Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E',>
         int rem, num;
         num = Arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex....");
+        System.out.println("Converting the Decimal Value " + num + " to Hex....>
 
         while(num != 0)
         {
@@ -18,6 +18,3 @@ public static int Arg1;
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
-    }
