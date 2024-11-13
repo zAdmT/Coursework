330db53cb63281b4cd979423bed4c10cc39c9c18
zAdmT <adamthomson079@gmail.com>
2024-11-13 18:50:35 +0000

    input catch added

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,7 +5,7 @@ class Dec2Hex
 public static int Arg1;
     public static void main(String args[])    {
         Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E',>
+        char ch[]={'0  ','1','2','3','4','5','6','7','8','9','A','B','C','D','E',>
         int rem, num;
         num = Arg1;
         String hexadecimal="";
